b82a35bbff6c2f2a7a067cd310a6344331c3ad48
翁克齊 <bob19870825@gmail.com>
2021-06-30 14:20:45 +0800

    Add new file

---
+++ 123.py
@@ -0,0 +1,17 @@
+# data types(種類)
+# 資料型別
+# 1.整數 integer int
+# 2.浮點數(小數) float
+# 3.布林值 boolean bool (True,False)
+# 4.字串 string str 'Allen'
+
+# price = 10
+# pen = 0.38
+# rain = True
+# name = 'Wally'
+
+# print(price, pen, rain, name)
+
+
+name = input('請輸入名字')
+print('HI', name)
